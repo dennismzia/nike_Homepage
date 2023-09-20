b4f7044ca192a8ceab9e800df3e4d9c01144abd2
Dennis malware <davspencer30@gmail.com>
2023-09-20 08:17:59 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -23,15 +23,15 @@ const App = () => (
     SpecialOffers
   </section>
 
-  <section className="padding">
+  <section className="bg-pale-blue padding">
     CustomerReviews
   </section>
 
-  <section className="padding">
+  <section className="padding-x sm:py-32 py-16 w-full">
     Subscribe
   </section>
 
-  <section className="padding">
+  <section className="bg-black padding-x padding-t pb-8">
     Footer
   </section>
 
