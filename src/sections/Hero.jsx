c3db3cf0e003c86fe33305c61e9d30d2c8a81d83
Dennis malware <davspencer30@gmail.com>
2023-09-20 15:03:58 +0300

    updates

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
 import  {arrowRight} from '../assets/icons';
+import { statistics} from '../constants';
 
 const Hero = () => (
   <section 
@@ -7,7 +8,7 @@ const Hero = () => (
   className="w-full xl:flex-row flex-col justify-center min-h-screen gap-10 max-container  border-2 border-red-500 p-2"
   >
     <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-       <p>Our summer Collection</p>
+       <p className="text-xl font-montserrat text-coral-red" >Our summer Collection</p>
        <h1>
         <span>The New Arrival</span>
         <br />
@@ -20,6 +21,16 @@ const Hero = () => (
         
        <Button label="Shop now"
        iconURL={arrowRight} />
+
+       <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+        {statistics.map((stat) => (
+          <div key={stat.label}>
+            <p>{stat.value}</p>
+            <p>{stat.label}</p>
+          </div>
+        ))}
+
+       </div>
 
     </div>
 
