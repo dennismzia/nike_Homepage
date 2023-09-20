e5b669accbc74c11f47a94b97382adbadacafb5a
Dennis malware <davspencer30@gmail.com>
2023-09-20 11:05:52 +0300

    updates

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,17 @@
-const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+import {headerLogo} from '../assets/images';
+import {hamburger} from '../assets/icons';
+
+const Nav = () => (
+        <header className="padding-x py-8 absolute z-10 w-full">
+            <nav>
+                <a href="/">
+                    <img src={headerLogo} alt="logo"
+                    width={130}
+                    height={29} />
+                </a>
+            </nav>
+        </header>
+    )
+
 
 export default Nav
