03bcf1c465d596809abf82d15121083a64db98c4
Dennis malware <davspencer30@gmail.com>
2023-09-20 14:52:15 +0300

    updates

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 
 const Button = ({label, iconURL}) => (
-    <button className="">
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
         {label}
 
         <img src={iconURL} alt="arrow right icon"
