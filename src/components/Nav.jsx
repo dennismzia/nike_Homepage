3708f4ce05bebd35f9c55292e7f1e9007cfa27ce
Dennis malware <davspencer30@gmail.com>
2023-09-20 13:51:10 +0300

    updates

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,10 +13,7 @@ const Nav = () => (
                 <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
                 {navLinks.map((item)=> (
                     <li key={item.label}>
-                        <a 
-                        href={item.href}
-                        className="font-montserrat leading-normal text-lg text-slate-700"
-                        >
+                        <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-700">
                             {item.label}
                         </a>
 
