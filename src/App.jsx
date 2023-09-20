4c9fc561356b7cbf18f9699774871f5ada3e7b44
Dennis malware <davspencer30@gmail.com>
2023-09-20 08:15:34 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,35 @@ const App = () => (
     <section className="xl:padding-1 wide:padding-r padding-b">
       Hero
     </section>
+
+  <section className="padding">
+    PopularProducts
+  </section>  
+
+  <section className="padding">
+    SuperQuality
+  </section>  
+  
+  <section className="padding-x py-10">
+    Services
+  </section>  
+  
+  <section className="padding">
+    SpecialOffers
+  </section>
+
+  <section className="padding">
+    CustomerReviews
+  </section>
+
+  <section className="padding">
+    Subscribe
+  </section>
+
+  <section className="padding">
+    Footer
+  </section>
+
   </main>
-)
-export default App
+);
+export default App 
