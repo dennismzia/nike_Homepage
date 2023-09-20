29b7276fb6f611b09b89efd65bf17d9000c1d3b6
Dennis malware <davspencer30@gmail.com>
2023-09-20 14:05:07 +0300

    fised some errors

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,14 +13,14 @@ const Nav = () => (
                 <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
                 {navLinks.map((item)=> (
                     <li key={item.label}>
-                        <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-700">
+                        <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">
                             {item.label}
                         </a>
 
                     </li>
                 ))}
                 </ul>
-                        
+
             </nav>
         </header>
     )
