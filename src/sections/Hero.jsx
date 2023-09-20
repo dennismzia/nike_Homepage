230f42354c72840eaaf1e0f39de774794baf0f30
Dennis malware <davspencer30@gmail.com>
2023-09-20 14:24:31 +0300

    updates

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,6 +4,10 @@ const Hero = () => (
   id="home"
   className="w-full xl:flex-row flex-col justify-center min-h-screen gap-10 max-container  border-2 border-red-500 p-2"
   >
+    <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      
+
+    </div>
 
   </section>
 )
