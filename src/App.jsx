f42c05e1aacbed76e04560f54e5b8cef5b8b00ed
Dennis malware <davspencer30@gmail.com>
2023-09-21 20:38:52 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,50 @@
-import {CustomerReviews,Footer,Hero,PopularProducts,Services,SpecialOffer,Subscribe,SuperQuality} from './sections'
+import React from 'react';
 import Nav from './components/Nav';
 
+import { Hero} from './sections';
+
+
+
 const App = () => (
-  <main className="relative">
+    <main className="relative">
+  
+      <Nav />
+      
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
 
-    <Nav />
+      </section>
+  
+    <section className="padding">
+      {/* <PopularProducts /> */}
+    </section>  
+  
+    <section className="padding">
+      {/* <SuperQuality /> */}
+    </section>  
     
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      <Hero />
+    <section className="padding-x py-10">
+      {/* <Services /> */}
+    </section>  
+    
+    <section className="padding">
+      {/* <SpecialOffer /> */}
     </section>
-
-  <section className="padding">
-    <PopularProducts />
-  </section>  
-
-  <section className="padding">
-    <SuperQuality />
-  </section>  
   
-  <section className="padding-x py-10">
-    <Services />
-  </section>  
+    <section className="bg-pale-blue padding">
+      {/* <CustomerReviews /> */}
+    </section>
   
-  <section className="padding">
-    <SpecialOffer />
-  </section>
-
-  <section className="bg-pale-blue padding">
-    <CustomerReviews />
-  </section>
-
-  <section className="padding-x sm:py-32 py-16 w-full">
-    <Subscribe />
-  </section>
-
-  <section className="bg-black padding-x padding-t pb-8">
-    <Footer />
-  </section>
-
-   </main> 
-
-  );
-export default App 
+    <section className="padding-x sm:py-32 py-16 w-full">
+      {/* <Subscribe /> */}
+    </section>
+  
+    <section className="bg-black padding-x padding-t pb-8">
+      {/* <Footer /> */}
+      footer
+    </section>
+  
+     </main> 
+  
+    );
+  export default App 
