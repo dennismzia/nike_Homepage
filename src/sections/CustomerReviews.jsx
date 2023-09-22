28ac122f75a87c85dbde7a6a436e9a52b193a54e
Dennis malware <davspencer30@gmail.com>
2023-09-22 19:05:59 +0300

    updates

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,11 +1,21 @@
+import {reviews} from '../constants';
 
 const CustomerReviews = () => {
   return (
     <section className="max-container">
 
       <h3 className="font-palanquin text-center text-4xl font-bold">
-        what Our <span> Customers</span> Say?
+        what Our <span className="text-coral-red"> Customers</span> Say?
       </h3>
+
+      <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stori from our satisfied customers about their exceptional experiences with us.</p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review)=>(
+          <ReviewCard />
+        ))}
+
+      </div>
     </section>
   )
 }
