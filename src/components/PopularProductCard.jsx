0def8c5df42f52bc0a06684f9768047fce89ae0c
Dennis malware <davspencer30@gmail.com>
2023-09-22 13:17:10 +0300

    updates

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const PopularProductCard = () => {
+const PopularProductCard = ({imgURL, name,price}) => {
   return (
-    <div>PopularProductCard</div>
+    <div className="flex flex-1">PopularProductCard</div>
   )
 }
 
