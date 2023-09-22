1186dc88dbe70cc390201ac6cd857ac3641841d3
Dennis malware <davspencer30@gmail.com>
2023-09-22 12:01:03 +0300

    updates

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -5,6 +5,13 @@ import {products} from '../constants';
 const PopularProducts = () => {
   return (
     <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2>Our popular products</h2>
+        <p>
+          Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design and value
+        </p>
+
+      </div>
 
     </section>
   )
